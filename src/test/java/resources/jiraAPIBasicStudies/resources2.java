7012f4770587b8556831aa93fd442a61dec8cffa
Giulio Faragalli <giuliojfaragallin@gmail.com>
2023-04-21 16:27:49 -0500

    Clases basicas y recursos de TestNG

---
+++ src/test/java/resources/jiraAPIBasicStudies/resources2.java
@@ -0,0 +1,17 @@
+package resources.jiraAPIBasicStudies;
+
+public class resources2 {
+
+	//You can call method from the class name with className.method if that method is defined with static
+	public static String placePostData()
+	{
+		
+		String res="/rest/api/2/issue/10207/comment/";
+		return res;
+	}
+	
+	
+	
+
+
+}
